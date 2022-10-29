750fc0b75a0ca5b14a847b7f8443cd931a464625
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-10-29 17:28:32 -0400

    Improve start speed, fix quick stop error

--- circlink/link.py
+++ circlink/link.py
@@ -8,12 +8,13 @@ Information and methods pertaining to links and link files.
 Author(s): Alec Delaney (Tekktrik)
 """
 
+import fcntl
 import json
 import os
 import pathlib
 import shutil
 import time
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 from typer import Exit
 from typing_extensions import TypeAlias
@@ -144,16 +145,21 @@ class CircuitPythonLink:
 
         # Save the object as a JSON file
         with open(save_filepath, mode="w", encoding="utf-8") as linkfile:
+            fcntl.lockf(linkfile, fcntl.LOCK_EX)
             json.dump(link_obj, linkfile, indent=4)
+            fcntl.lockf(linkfile, fcntl.LOCK_UN)
 
         # Return the save filepath
         return pathlib.Path(save_filepath)
 
     @classmethod
-    def load_link_by_filepath(cls, link_filepath: str) -> "CircuitPythonLink":
+    def load_link_by_filepath(cls, link_filepath: str) -> Optional["CircuitPythonLink"]:
         """Create a CircuitPythonLink from a JSON file, by filepath."""
         with open(link_filepath, encoding="utf-8") as linkfile:
-            link_obj = json.load(linkfile)
+            link_contents = linkfile.read()
+            if not link_contents:
+                return None
+            link_obj = json.loads(link_contents)
 
         link = cls(
             name=link_obj["name"],
