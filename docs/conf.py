829f32f7a595cd5e6cee915c77613a874dcbd21d
Alec Delaney <tekktrik@gmail.com>
2024-10-27 14:49:22 -0400

    Update for sphinx-rtd-theme update

--- docs/conf.py
+++ docs/conf.py
@@ -34,5 +34,4 @@ exclude_patterns = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), "_static"]
 html_static_path = ["_static"]
